e4e50ac228acd45b5bc165807070ea74ceb3afc3
Google Cloud SDK Mirror <cloudsdk.mirror@gmail.com>
2017-09-21 17:56:59 +0900

    1.9.26

--- goroot/src/runtime/zversion.go
+++ goroot/src/runtime/zversion.go
@@ -3,5 +3,5 @@
 package runtime
 
 const defaultGoroot = `/private/var/folders/00/0v42r000h01000cxqpysvccm003chb/T/appengine/go_appengine/goroot`
-const theVersion = `go1.4.2 (appengine-1.9.25)`
+const theVersion = `go1.4.2 (appengine-1.9.26)`
 var buildVersion = theVersion
