098be8a664fb4e5881b441e7a14c46ea57de4f9e
Google Cloud SDK Mirror <cloudsdk.mirror@gmail.com>
2017-09-22 17:39:52 +0900

    1.9.56

--- goroot-1.6/src/cmd/internal/obj/zbootstrap.go
+++ goroot-1.6/src/cmd/internal/obj/zbootstrap.go
@@ -4,12 +4,12 @@ package obj
 
 import "runtime"
 
-const defaultGOROOT = `/tmp/go_sdk278153215/appengine/go_appengine/goroot-1.6`
+const defaultGOROOT = `/tmp/go_sdk886933799/appengine/go_appengine/goroot-1.6`
 const defaultGO386 = `sse2`
 const defaultGOARM = `5`
 const defaultGOOS = runtime.GOOS
 const defaultGOARCH = runtime.GOARCH
 const defaultGO_EXTLINK_ENABLED = ``
-const version = `1.6.4 (appengine-1.9.56)`
+const version = `1.6.4 (appengine-1.9.57)`
 const stackGuardMultiplier = 1
 const goexperiment = ``
--- goroot-1.8/src/runtime/internal/sys/zversion.go
+++ goroot-1.8/src/runtime/internal/sys/zversion.go
@@ -2,8 +2,8 @@
 
 package sys
 
-const DefaultGoroot = `/tmp/go_sdk278153215/appengine/go_appengine/goroot-1.8`
-const TheVersion = `1.8.3 (appengine-1.9.56)`
+const DefaultGoroot = `/tmp/go_sdk886933799/appengine/go_appengine/goroot-1.8`
+const TheVersion = `1.8.3 (appengine-1.9.57)`
 const Goexperiment = ``
 const StackGuardMultiplier = 1
 
